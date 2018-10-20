022a1befd2a47ae2cb88e1b06786b5cc45617c0d
Ethan Daniel <ethancdaniel@gmail.com>
2018-10-19 21:42:42 -0700

    MVC / key-value pair data storage

---
+++ Todoey/Models/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Ethan Daniel on 10/8/18.
+//  Copyright © 2018 Ethan Daniel. All rights reserved.
+//
+
+import Foundation
+
+class Item: Codable {
+    var itemText = ""
+    var done = false
+}
